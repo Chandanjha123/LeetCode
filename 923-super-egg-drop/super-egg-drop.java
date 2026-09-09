9331a6763420921aad73db2ab010b6d1e471d8a9
CHANDAN JHA <126557619+Chandanjha123@users.noreply.github.com>
2026-09-10 01:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (78.84%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,56 +1,17 @@
 class Solution {
-    public int superEggDrop(int n, int k) {
-        int dp[][] = new int[n + 1][k + 1];
+    public int superEggDrop(int k, int n) {
+        long[] dp = new long[k + 1];
+        int moves = 0;
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        while (dp[k] < n) {
+            moves++;
 
-        return solve(n, k, dp);
-    }
-
-    int solve(int n, int f, int[][] dp) {
-
-        // Base cases
-        if (n == 1) {
-            return f;
-        }
-
-        if (f == 0 || f == 1) {
-            return f;
-        }
-
-        // Already calculated
-        if (dp[n][f] != -1) {
-            return dp[n][f];
-        }
-
-        int low = 1;
-        int high = f;
-
-        int mn = Integer.MAX_VALUE;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            int eggBreaks = solve(n - 1, mid - 1, dp);
-
-            int eggNotBreaks = solve(n, f - mid, dp);
-
-            int temp = 1 + Math.max(eggBreaks, eggNotBreaks);
-
-            mn = Math.min(mn, temp);
-
-            if (eggBreaks < eggNotBreaks) {
-                // Need to go higher
-                low = mid + 1;
-            } else {
-                // Need to go lower
-                high = mid - 1;
+            for (int i = k; i >= 1; i--) {
+                dp[i] = dp[i] + dp[i - 1] + 1;
             }
         }
 
-        return dp[n][f] = mn;
+        return moves;
+
     }
 }
